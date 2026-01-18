47c5398e9454cb7bd76530804f79da948c03dded
Vitoria Carolina <92760249+vitoriadeveloper@users.noreply.github.com>
2026-01-18 19:15:00 -0300

    [FEAT] - add address model

---
+++ vifood/src/main/java/com/vitoriadeveloper/vifood/domain/model/Address.java
@@ -0,0 +1,32 @@
+package com.vitoriadeveloper.vifood.domain.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Embeddable
+public class Address {
+    @Column(name = "endereco_cep")
+    private String cep;
+
+    @Column(name = "endereco_logradouro")
+    private String logradouro;
+
+    @Column(name = "endereco_numero")
+    private String numero;
+
+    @Column(name = "endereco_complemento")
+    private String complemento;
+
+    @Column(name = "endereco_bairro")
+    private String bairro;
+
+    @ManyToOne
+    @JoinColumn(name = "endereco_cidade_id")
+    private City cidade;
+}

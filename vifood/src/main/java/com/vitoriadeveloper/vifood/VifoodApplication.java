0f73b229b2f073a4ed25861a9e26855fb91ded1c
Vitoria Carolina <92760249+vitoriadeveloper@users.noreply.github.com>
2026-02-06 19:32:01 -0300

    feat: add logger

--- vifood/src/main/java/com/vitoriadeveloper/vifood/VifoodApplication.java
+++ vifood/src/main/java/com/vitoriadeveloper/vifood/VifoodApplication.java
@@ -1,16 +1,21 @@
 package com.vitoriadeveloper.vifood;
 
 import com.vitoriadeveloper.vifood.infra.adapters.repositories.CustomJpaAdapter;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.slf4j.Logger;
 
 @SpringBootApplication
 @EnableJpaRepositories(repositoryBaseClass = CustomJpaAdapter.class)
 public class VifoodApplication {
+	private static final Logger log = LoggerFactory.getLogger(VifoodApplication.class);
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(VifoodApplication.class, args);
+		log.info("Aplicaçao iniciada com sucesso !!");
 	}
 
 }

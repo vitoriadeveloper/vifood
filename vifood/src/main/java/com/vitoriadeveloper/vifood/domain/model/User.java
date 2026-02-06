c31fb2c6ad4e767bc5cfaca3bdaa42f4b9fe872c
Vitoria Carolina <92760249+vitoriadeveloper@users.noreply.github.com>
2026-02-06 19:31:15 -0300

    chore: add  one to many at user model

--- vifood/src/main/java/com/vitoriadeveloper/vifood/domain/model/User.java
+++ vifood/src/main/java/com/vitoriadeveloper/vifood/domain/model/User.java
@@ -3,8 +3,11 @@ package com.vitoriadeveloper.vifood.domain.model;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Entity
 @Table(name = "tb_usuarios")
@@ -23,6 +26,12 @@ public class User {
 
     private String senha;
 
+    @CreationTimestamp
     @Column(name = "data_cadastro")
     private Date dataCadastro;
+
+    @ManyToMany
+    @JoinTable(name = "tb_usuario_grupo", joinColumns = @JoinColumn(name = "usuario_id"),
+            inverseJoinColumns = @JoinColumn(name = "grupo_id"))
+    private List<GroupPermission> grupos = new ArrayList<>();
 }

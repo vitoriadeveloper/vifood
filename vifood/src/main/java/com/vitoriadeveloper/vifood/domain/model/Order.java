7f1bb95a3e4b33357d4198a423c86f0ba4b75124
Vitoria Carolina <92760249+vitoriadeveloper@users.noreply.github.com>
2026-02-07 21:27:45 -0300

    feat: add Order entity and update Flyway configuration for test data

---
+++ vifood/src/main/java/com/vitoriadeveloper/vifood/domain/model/Order.java
@@ -0,0 +1,39 @@
+package com.vitoriadeveloper.vifood.domain.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name = "tb_pedidos")
+@Getter
+@Setter
+public class Order {
+
+    @Id
+    private Long id;
+
+    @Column(name = "id_cliente", nullable = false)
+    private Integer clienteId;
+
+    @Column(name = "data_pedido", nullable = false)
+    private Date dataPedido;
+
+    @Column(name = "valor_total", nullable = false)
+    private BigDecimal valorTotal;
+
+    @Column(name = "status", nullable = false)
+    private String status;
+
+    @OneToMany(
+            mappedBy = "pedido",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true
+    )
+    private List<OrderItem> itens = new ArrayList<>();
+}

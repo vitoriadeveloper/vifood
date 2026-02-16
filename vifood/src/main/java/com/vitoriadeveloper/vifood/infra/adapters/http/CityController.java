329dbc3cc1017c6cbb09ae5578cb60f111ade965
Vitoria Carolina <92760249+vitoriadeveloper@users.noreply.github.com>
2026-02-16 11:46:19 -0300

    refactor: city controller

--- vifood/src/main/java/com/vitoriadeveloper/vifood/infra/adapters/http/CityController.java
+++ vifood/src/main/java/com/vitoriadeveloper/vifood/infra/adapters/http/CityController.java
@@ -3,7 +3,6 @@ package com.vitoriadeveloper.vifood.infra.adapters.http;
 import com.vitoriadeveloper.vifood.application.services.CityService;
 import com.vitoriadeveloper.vifood.domain.model.City;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;

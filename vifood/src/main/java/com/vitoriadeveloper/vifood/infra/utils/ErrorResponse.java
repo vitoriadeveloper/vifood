cd96ef0bc465163824a3ea8c3c3a92b506329958
Vitoria Carolina <92760249+vitoriadeveloper@users.noreply.github.com>
2026-02-16 21:53:47 -0300

    feat: blocking to see properties null in response

--- vifood/src/main/java/com/vitoriadeveloper/vifood/infra/utils/ErrorResponse.java
+++ vifood/src/main/java/com/vitoriadeveloper/vifood/infra/utils/ErrorResponse.java
@@ -1,7 +1,10 @@
 package com.vitoriadeveloper.vifood.infra.utils;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import java.time.OffsetDateTime;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record ErrorResponse(
         OffsetDateTime timestamp,
         int status,

9479fd8412deec659f24e500daeeaf5ec887e4ec
Vitoria Carolina <92760249+vitoriadeveloper@users.noreply.github.com>
2026-02-18 11:04:36 -0300

    feat: add fields properties at exception record

--- vifood/src/main/java/com/vitoriadeveloper/vifood/infra/utils/ErrorResponse.java
+++ vifood/src/main/java/com/vitoriadeveloper/vifood/infra/utils/ErrorResponse.java
@@ -1,16 +1,23 @@
 package com.vitoriadeveloper.vifood.infra.utils;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
 
 import java.time.OffsetDateTime;
+import java.util.List;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public record ErrorResponse(
         OffsetDateTime timestamp,
         int status,
         String error,
-        String message
+        String message,
+        List<Field> fields
 ) {
-    public void error(String message) {
+
+    @Builder
+    public record Field(
+            String name,
+            String userMessage) {
     }
 }

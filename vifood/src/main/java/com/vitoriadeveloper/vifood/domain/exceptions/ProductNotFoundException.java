51819815de9316db20a85f6894a0f14b064fd012
Vitoria Carolina <92760249+vitoriadeveloper@users.noreply.github.com>
2026-02-26 13:17:45 -0300

    feat: add new method at exception

--- vifood/src/main/java/com/vitoriadeveloper/vifood/domain/exceptions/ProductNotFoundException.java
+++ vifood/src/main/java/com/vitoriadeveloper/vifood/domain/exceptions/ProductNotFoundException.java
@@ -7,4 +7,8 @@ public class ProductNotFoundException extends RuntimeException {
     public ProductNotFoundException(UUID id) {
         super("Produto de id: " + id + " não encontrado!");
     }
+
+        public ProductNotFoundException(UUID productId, UUID restaurantId) {
+            super("Produto de id: " + productId + " não encontrado para o restaurante de id: " + restaurantId);
+        }
 }

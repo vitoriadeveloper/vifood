367cfbbfc113eab5daf13e3e122d6a74e62e8b1a
Vitoria Carolina <92760249+vitoriadeveloper@users.noreply.github.com>
2026-02-28 21:02:25 -0300

    feat: implementing new methods in groupPermission port

--- vifood/src/main/java/com/vitoriadeveloper/vifood/domain/ports/in/IGroupPermissionUseCasePort.java
+++ vifood/src/main/java/com/vitoriadeveloper/vifood/domain/ports/in/IGroupPermissionUseCasePort.java
@@ -1,6 +1,7 @@
 package com.vitoriadeveloper.vifood.domain.ports.in;
 
 import com.vitoriadeveloper.vifood.domain.exceptions.GroupPermissionNotFoundException;
+import com.vitoriadeveloper.vifood.domain.exceptions.PermissionNotFoundException;
 import com.vitoriadeveloper.vifood.domain.model.GroupPermission;
 
 import java.util.List;
@@ -17,4 +18,8 @@ public interface IGroupPermissionUseCasePort {
     void updateById(UUID id, GroupPermission groupPermission) throws GroupPermissionNotFoundException;
 
     GroupPermission save(GroupPermission groupPermission);
+
+    GroupPermission findPermissionByGroupId(UUID groupId, UUID permissionId) throws GroupPermissionNotFoundException, PermissionNotFoundException;
+    void associateGroupWithPermission(UUID groupId, UUID permissionId) throws GroupPermissionNotFoundException, PermissionNotFoundException;
+    void disassociateGroupWithPermission(UUID groupId, UUID permissionId) throws GroupPermissionNotFoundException, PermissionNotFoundException;
 }

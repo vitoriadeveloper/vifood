8f7eb309c190cf6958bab1445dd78044e0b7b6a7
Vitoria Carolina <92760249+vitoriadeveloper@users.noreply.github.com>
2026-03-01 15:10:31 -0300

    feat: implementing permission controller

---
+++ vifood/src/main/java/com/vitoriadeveloper/vifood/infra/adapters/http/PermissionController.java
@@ -0,0 +1,51 @@
+package com.vitoriadeveloper.vifood.infra.adapters.http;
+
+
+import com.vitoriadeveloper.vifood.application.services.GroupPermissionService;
+import com.vitoriadeveloper.vifood.application.services.PermissionService;
+import com.vitoriadeveloper.vifood.domain.exceptions.PermissionNotFoundException;
+import com.vitoriadeveloper.vifood.infra.adapters.model.dto.request.CreatePermissionRequest;
+import com.vitoriadeveloper.vifood.infra.adapters.model.dto.request.GroupCreatePermissionRequest;
+import com.vitoriadeveloper.vifood.infra.adapters.model.dto.response.GroupPermissionResponse;
+import com.vitoriadeveloper.vifood.infra.adapters.model.dto.response.PermissionResponse;
+import com.vitoriadeveloper.vifood.infra.adapters.model.mapper.GroupPermissionMapper;
+import com.vitoriadeveloper.vifood.infra.adapters.model.mapper.PermissionMapper;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/vifood/permissoes")
+@RequiredArgsConstructor
+public class PermissionController {
+    private final PermissionService service;
+
+    @GetMapping
+    public List<PermissionResponse> findAllPermissionsOfGroup() {
+        var permissions = service.findAll();
+
+        return PermissionMapper.toResponseList(permissions);
+    }
+
+    @DeleteMapping("/{permissaoId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void removingPermission(
+            @PathVariable UUID permissaoId
+    ) {
+        service.delete(permissaoId);
+    }
+
+    @PostMapping
+    public PermissionResponse createPermission(@Valid @RequestBody CreatePermissionRequest request) {
+        var permission = PermissionMapper.toDomain(request);
+
+        var saved = service.save(permission);
+
+        return PermissionMapper.toResponse(saved);
+    }
+
+}

9558e84cc8111a2b19fe38e6f52ca19b135e2378
Vitoria Carolina <92760249+vitoriadeveloper@users.noreply.github.com>
2026-02-28 21:00:39 -0300

    fix: model entity of userPermission

--- vifood/src/main/java/com/vitoriadeveloper/vifood/infra/repositories/UserPermissionRepository.java
+++ vifood/src/main/java/com/vitoriadeveloper/vifood/infra/repositories/UserPermissionRepository.java
@@ -1,10 +1,10 @@
 package com.vitoriadeveloper.vifood.infra.repositories;
 
-import com.vitoriadeveloper.vifood.domain.model.PaymentMethod;
+import com.vitoriadeveloper.vifood.domain.model.UserPermission;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface UserPermissionRepository extends CustomJpaRepository<PaymentMethod, UUID> {
+public interface UserPermissionRepository extends CustomJpaRepository<UserPermission, UUID> {
 }

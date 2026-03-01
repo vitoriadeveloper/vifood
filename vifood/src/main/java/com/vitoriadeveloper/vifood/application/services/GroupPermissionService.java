3b52e0ef687175746fedb2f56a8af79764ae1a3f
Vitoria Carolina <92760249+vitoriadeveloper@users.noreply.github.com>
2026-02-28 21:01:15 -0300

    feat: implementing addPermission and removePermission in group service

--- vifood/src/main/java/com/vitoriadeveloper/vifood/application/services/GroupPermissionService.java
+++ vifood/src/main/java/com/vitoriadeveloper/vifood/application/services/GroupPermissionService.java
@@ -1,15 +1,16 @@
 package com.vitoriadeveloper.vifood.application.services;
 
 import com.vitoriadeveloper.vifood.domain.exceptions.GroupPermissionNotFoundException;
+import com.vitoriadeveloper.vifood.domain.exceptions.PermissionNotFoundException;
 import com.vitoriadeveloper.vifood.domain.model.GroupPermission;
 import com.vitoriadeveloper.vifood.domain.ports.in.IGroupPermissionUseCasePort;
 import com.vitoriadeveloper.vifood.domain.ports.out.IGroupPermissionRepositoryPort;
+import com.vitoriadeveloper.vifood.domain.ports.out.IPermissionRepositoryPort;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 @Validated
@@ -17,6 +18,7 @@ import java.util.UUID;
 @AllArgsConstructor
 public class GroupPermissionService implements IGroupPermissionUseCasePort {
     private final IGroupPermissionRepositoryPort repository;
+    private final IPermissionRepositoryPort permissionRepository;
 
 
     @Override
@@ -38,12 +40,45 @@ public class GroupPermissionService implements IGroupPermissionUseCasePort {
 
     @Override
     public void updateById(UUID id, GroupPermission groupPermission) throws GroupPermissionNotFoundException {
-        repository.findById(id).orElseThrow(() -> new GroupPermissionNotFoundException(id));
-        repository.updateById(id, groupPermission);
+        GroupPermission group = repository.findById(id).orElseThrow(() -> new GroupPermissionNotFoundException(id));
+
+        if (groupPermission.getNome() != null) {
+            group.setNome(groupPermission.getNome());
+        }
+        repository.save(group);
     }
 
     @Override
     public GroupPermission save(GroupPermission groupPermission) {
         return repository.save(groupPermission);
     }
+
+    @Override
+    public GroupPermission findPermissionByGroupId(UUID groupId, UUID permissionId) throws GroupPermissionNotFoundException, PermissionNotFoundException {
+        repository.findById(groupId).orElseThrow(() -> new GroupPermissionNotFoundException(groupId));
+        return repository.findByIdAndPermissionId(groupId, permissionId).orElseThrow(() -> new PermissionNotFoundException(permissionId));
+    }
+
+    @Override
+    public void associateGroupWithPermission(UUID groupId, UUID permissionId) throws GroupPermissionNotFoundException, PermissionNotFoundException {
+        var group = repository.findById(groupId).orElseThrow(() -> new GroupPermissionNotFoundException(groupId));
+
+        var permission = permissionRepository.findById(permissionId).orElseThrow(() -> new PermissionNotFoundException(permissionId));
+
+        group.addPermission(permission);
+
+        repository.save(group);
+    }
+
+    @Override
+    public void disassociateGroupWithPermission(UUID groupId, UUID permissionId) throws GroupPermissionNotFoundException, PermissionNotFoundException {
+        var group = repository.findById(groupId).orElseThrow(() -> new GroupPermissionNotFoundException(groupId));
+
+        var permission = permissionRepository.findById(permissionId).orElseThrow(() -> new PermissionNotFoundException(permissionId));
+
+        group.removePermission(permission);
+
+        repository.save(group);
+    }
+
 }

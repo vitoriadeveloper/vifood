c2429c7928092fbc903f47b10a90024903ab11b6
Vitoria Carolina <92760249+vitoriadeveloper@users.noreply.github.com>
2026-03-06 16:01:14 -0300

    feat: implementing order jpa adapter

---
+++ vifood/src/main/java/com/vitoriadeveloper/vifood/infra/adapters/repositories/OrderJpaAdapter.java
@@ -0,0 +1,37 @@
+    package com.vitoriadeveloper.vifood.infra.adapters.repositories;
+
+    import com.vitoriadeveloper.vifood.domain.model.Order;
+    import com.vitoriadeveloper.vifood.domain.ports.out.IOrderRepositoryPort;
+    import com.vitoriadeveloper.vifood.infra.repositories.OrderRepository;
+    import lombok.RequiredArgsConstructor;
+    import org.springframework.stereotype.Component;
+
+    import java.util.List;
+    import java.util.Optional;
+    import java.util.UUID;
+
+    @Component
+    @RequiredArgsConstructor
+    public class OrderJpaAdapter implements IOrderRepositoryPort {
+        private final OrderRepository repository;
+
+        @Override
+        public Optional<Order> findById(UUID id) {
+            return repository.findById(id);
+        }
+
+        @Override
+        public List<Order> findAll() {
+            return repository.findAll();
+        }
+
+        @Override
+        public Order save(Order order) {
+            return repository.save(order);
+        }
+
+        @Override
+        public void delete(Order order) {
+            repository.delete(order);
+        }
+    }

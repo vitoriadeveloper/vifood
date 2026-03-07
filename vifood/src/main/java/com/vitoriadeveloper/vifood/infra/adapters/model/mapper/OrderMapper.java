207cb328557aa1acb5f8d3bde14be0b3812fbfff
Vitoria Carolina <92760249+vitoriadeveloper@users.noreply.github.com>
2026-03-06 21:49:24 -0300

    feat: implementing order mapper

---
+++ vifood/src/main/java/com/vitoriadeveloper/vifood/infra/adapters/model/mapper/OrderMapper.java
@@ -0,0 +1,77 @@
+package com.vitoriadeveloper.vifood.infra.adapters.model.mapper;
+
+import com.vitoriadeveloper.vifood.domain.model.*;
+import com.vitoriadeveloper.vifood.domain.model.enums.OrderStatus;
+import com.vitoriadeveloper.vifood.infra.adapters.model.dto.request.CreateOrderRequest;
+import com.vitoriadeveloper.vifood.infra.adapters.model.dto.request.UpdateOrderRequest;
+import com.vitoriadeveloper.vifood.infra.adapters.model.dto.response.OrderResponse;
+
+public class OrderMapper {
+
+    public static OrderResponse toOrderResponse(Order order) {
+        return new OrderResponse(
+                order.getId(),
+                order.getDataPedido(),
+                order.getValorTotal(),
+                order.getStatus().name(),
+                RestaurantMapper.toRestaurantSummaryResponse(order.getRestaurante()),
+                UserMapper.toClientSummaryResponse(order.getCliente()),
+                AddressMapper.toResponse(order.getEnderecoEntrega()),
+                ItemsMapper.toItemsResponse(order.getItens())
+        );
+    }
+
+    public static Order toDomain(CreateOrderRequest request) {
+        Order order = new Order();
+        Restaurant restaurant = new Restaurant();
+        restaurant.setId(request.restaurantId());
+        User client = new User();
+        client.setId(request.clientId());
+
+        order.setRestaurante(restaurant);
+        order.setCliente(client);
+        order.setEnderecoEntrega(AddressMapper.toDomain(request.enderecoEntrega()));
+
+        request.itens().forEach(item -> {
+            OrderItem itemOrder = new OrderItem();
+
+            Product product = new Product();
+            product.setId(item.productId());
+
+            itemOrder.setProduto(product);
+            itemOrder.setQuantidade(item.quantidade());
+            itemOrder.setObservacao(item.observacao());
+
+            order.addItem(itemOrder);
+        });
+        return order;
+
+    }
+
+    public static Order toDomain(UpdateOrderRequest request) {
+        Order order = new Order();
+
+        if (request.enderecoEntrega() != null) {
+            order.setEnderecoEntrega(
+                    AddressMapper.toDomain(request.enderecoEntrega())
+            );
+        }
+
+        if (request.itens() != null) {
+            request.itens().forEach(item -> {
+                OrderItem orderItem = new OrderItem();
+
+                Product product = new Product();
+                product.setId(item.productId());
+
+                orderItem.setProduto(product);
+                orderItem.setQuantidade(item.quantidade());
+                orderItem.setObservacao(item.observacao());
+
+                order.addItem(orderItem);
+            });
+        }
+
+        return order;
+    }
+}

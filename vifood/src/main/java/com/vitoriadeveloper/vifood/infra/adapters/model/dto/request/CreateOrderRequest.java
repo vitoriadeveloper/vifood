fcdbc935b5c82979c780c7749e086db95c4bdb04
Vitoria Carolina <92760249+vitoriadeveloper@users.noreply.github.com>
2026-03-07 19:23:13 -0300

    refactor: changing names

--- vifood/src/main/java/com/vitoriadeveloper/vifood/infra/adapters/model/dto/request/CreateOrderRequest.java
+++ vifood/src/main/java/com/vitoriadeveloper/vifood/infra/adapters/model/dto/request/CreateOrderRequest.java
@@ -3,15 +3,14 @@ package com.vitoriadeveloper.vifood.infra.adapters.model.dto.request;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 
-import java.math.BigDecimal;
 import java.util.List;
 import java.util.UUID;
 
 public record CreateOrderRequest(
         @NotNull
-        UUID restaurantId,
+        UUID restauranteId,
         @NotNull
-        UUID clientId,
+        UUID clienteId,
         @Valid
         AddressCreateRequest enderecoEntrega,
         @Valid

3077221cac8dcd1ec4c522af44e75ded920e4cf9
Vitoria Carolina <92760249+vitoriadeveloper@users.noreply.github.com>
2026-03-06 21:47:16 -0300

    feat: implementing items mapper and respnse

---
+++ vifood/src/main/java/com/vitoriadeveloper/vifood/infra/adapters/model/dto/response/ItemsResponse.java
@@ -0,0 +1,11 @@
+package com.vitoriadeveloper.vifood.infra.adapters.model.dto.response;
+
+import java.math.BigDecimal;
+
+public record ItemsResponse(
+        Integer quantidade,
+        BigDecimal precoTotal,
+        ProductSummaryResponse produto
+
+) {
+}

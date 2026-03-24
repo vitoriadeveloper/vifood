ff60de0c5eb99bb75cca6040bffbb7c514ffb127
Vitoria Carolina <92760249+vitoriadeveloper@users.noreply.github.com>
2026-03-24 07:23:07 -0300

    feat: statistics service implemented

---
+++ vifood/src/main/java/com/vitoriadeveloper/vifood/application/services/StatisticsService.java
@@ -0,0 +1,22 @@
+package com.vitoriadeveloper.vifood.application.services;
+
+import com.vitoriadeveloper.vifood.domain.exceptions.RestaurantNotFoundException;
+import com.vitoriadeveloper.vifood.domain.filters.StatisticsFilter;
+import com.vitoriadeveloper.vifood.domain.model.DailySales;
+import com.vitoriadeveloper.vifood.domain.ports.in.IStatisticsUseCasePort;
+import com.vitoriadeveloper.vifood.domain.ports.out.IStatisticsRepositoryPort;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class StatisticsService implements IStatisticsUseCasePort {
+    private final IStatisticsRepositoryPort repository;
+
+    @Override
+    public List<DailySales> getDailySales(StatisticsFilter filters) throws RestaurantNotFoundException {
+        return repository.getDailySalesByRestaurantId(filters);
+    }
+}
